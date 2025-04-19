513585761afabb9e9a9b998957a67c543d264118
killianoc1 <killianoc13@gmail.com>
2025-04-19 01:29:01 +0100

    First iteration of Control Class for instantiating and printing frequency table

--- ControlCA2.java
+++ ControlCA2.java
@@ -1,6 +1,6 @@
 // SensorIsTriggered
 
-import java.util.ArrayList;
+import java.util.Map;
 
 public class ControlCA2 
 {
@@ -9,17 +9,20 @@ public class ControlCA2
         fileProcessor processor = new fileProcessor();
         processor.connectToFile();
 
-        // Part 2
-        ArrayList<String> rows = processor.readFile();
-        for (String row : rows)
+        // Part 2: Read and print the frequency table
+        Map<String, Map<String, Integer>> frequencyTable = processor.getFrequencyTable();
+        
+        // Print the frequency table
+        System.out.println("\nFrequency Table for sensorTriggered_data.csv:\n");
+        for (String column : frequencyTable.keySet()) 
         {
-            System.out.println(row);
+            System.out.println(column + ":");
+            Map<String, Integer> frequencies = frequencyTable.get(column);
+            for (Map.Entry<String, Integer> entry : frequencies.entrySet()) 
+            {
+                System.out.printf("  %-15s : %d%n", entry.getKey(), entry.getValue());
+            }
+            System.out.println();
         }
-
-        // Part 3
-        // processor.getFileWriter();  
-        // processor.writeLineToFile("Test1,Test2,Test3,Test4");
-        // processor.writeLineToFile("Demo1,Demo2,Demo3,Demo4");  
-        // processor.closeWriteFile(); 
     }
 }
